d5a1c5c179c17308613831d19c7efb7ec57ba8f4
Ehicodes <osaigbovoehinomen@gmail.com>
2025-01-31 21:36:24 +0100

    coding challenge 2. task 1

--- challenges.js
+++ challenges.js
@@ -22,7 +22,7 @@ Hints:
 § To calculate average of 3 values, add them all together and divide by 3
 § To check if number A is at least double number B, check for A >= 2 * B.
 Apply this to the team's average scores 😉
-GOOD */
+GOOD 
 
 //FUNCTION TO CALCULATE AVERAGE SCORE OF BOTH TEAMS.
 // DATA 1
@@ -87,3 +87,31 @@ checkWinner(dolphinsAvg2, koalasAvg2);
 
 // console.log(scoreDolphins, scoreKoalas);
 // checkWinner(scoreDolphins, scoreKoalas);
+*/
+
+/* CODING CHALLENGE 2.
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44 */
+
+const calcTip = function (billValue) {
+  if (billValue >= 50 && billValue <= 300) {
+    return billValue * 0.15;
+  } else {
+    return billValue * 0.2;
+  }
+};
+
+const test1 = calcTip(100);
+console.log(test1);
