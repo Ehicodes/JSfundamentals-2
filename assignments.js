ac60f34f321301c912dd0466e980707f72c2ddac
Ehicodes <osaigbovoehinomen@gmail.com>
2025-02-07 22:13:28 +0100

    assignment 9. task 1

--- assignments.js
+++ assignments.js
@@ -193,26 +193,42 @@ const myCountry = {
   languages: "Yoruba",
   population: 223.8,
   neighbours: ["Benin", "Chad", "Niger"],
+
+  describe: function () {
+    return `${this.country} has ${this.population} million ${this.languages}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called, ${this.capital}`;
+  },
 };
 
+// // console.log(myCountry);
+
+// /* ASSIGNMENT 8. Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the
+// console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+// and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then
+// decrease it by two million using brackets notation. */
+
+// console.log(
+//   `${myCountry.country} has ${myCountry.population} million ${myCountry.languages}-speaking people,  ${myCountry.neighbours.length} neighbouring countries and a capital called, ${myCountry.capital}`
+// );
+
+// // console.log(myCountry.population + 2);
+// // console.log(myCountry["population"] - 2);
+
+// //tutor code
+// myCountry.population += 2;
+// console.log(myCountry);
+// myCountry.population -= 2;
 // console.log(myCountry);
 
-/* ASSIGNMENT 8. Dot vs. Bracket Notation
-1. Using the object from the previous assignment, log a string like this to the
-console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
-and a capital called Helsinki.'
-2. Increase the country's population by two million using dot notation, and then
-decrease it by two million using brackets notation. */
+/* ASSIGNMENT 9. Object Methods.
+1. Add a method called 'describe' to the 'myCountry' object. This method
+will log a string to the console, similar to the string logged in the previous
+assignment, but this time using the 'this' keyword.
+2. Call the 'describe' method
+3. Add a method called 'checkIsland' to the 'myCountry' object. This
+method will set a new property on the object, called 'isIsland'.
+'isIsland' will be true if there are no neighbouring countries, and false if
+there are. Use the ternary operator to set the property. */
 
-console.log(
-  `${myCountry.country} has ${myCountry.population} million ${myCountry.languages}-speaking people,  ${myCountry.neighbours.length} neighbouring countries and a capital called, ${myCountry.capital}`
-);
-
-// console.log(myCountry.population + 2);
-// console.log(myCountry["population"] - 2);
-
-//tutor code
-myCountry.population += 2;
-console.log(myCountry);
-myCountry.population -= 2;
-console.log(myCountry);
+console.log(myCountry.describe());
