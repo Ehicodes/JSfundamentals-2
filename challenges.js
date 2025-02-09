ca4cdcb2756c0b1b8f236cd41b2f511b3745f352
Ehicodes <osaigbovoehinomen@gmail.com>
2025-02-09 20:36:29 +0100

    coding challenge 3. task 2(methods added to each objects)

--- challenges.js
+++ challenges.js
@@ -176,12 +176,24 @@ const mark = {
   fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
+
+  calcBMI: function () {
+    return this.mass / (this.height * this.height);
+  },
 };
 
 const john = {
   fullName: "John Smith",
   mass: 92,
   height: 1.95,
+
+  calcBMI: function () {
+    return this.mass / (this.height * this.height);
+  },
 };
 
 console.log(mark, john);
+
+//TASK 2.
+console.log(mark.calcBMI());
+console.log(john.calcBMI());
